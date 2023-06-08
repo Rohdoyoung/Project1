7d3811c922e8d2273c7405ff408321edae09c3cf
RohDoyoung <rdyoung82@hanmail.net>
2023-06-08 14:59:01 +0900

    커밋 4 나누기 추가

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def sub_func(n1, n2):
 def mul_func(n1, n2):
     return n1 * n2
 
+def div_func(n1, n2):
+    return n1 / n2
+
 num1, num2, res = 100, 200, 0
 
 res = add_func(num1, num2)
@@ -17,3 +20,6 @@ print(num1, " - ", num2, " = ", res)
 
 res = mul_func(num1, num2)
 print(num1, " * ", num2, " = ", res)
+
+res = div_func(num1, num2)
+print(num1, " / ", num2, " = ", res)
